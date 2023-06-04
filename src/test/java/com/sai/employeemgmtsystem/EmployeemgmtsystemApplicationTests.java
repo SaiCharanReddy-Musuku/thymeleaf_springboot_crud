172808b07d597746d714f168c97df71ac35d4dd0
SaiCharanReddyMusuku <saicharanreddymusuku@gmail.com>
2023-06-04 16:31:45 -0400

    Updated main test class file

--- src/test/java/com/sai/employeemgmtsystem/EmployeemgmtsystemApplicationTests.java
+++ src/test/java/com/sai/employeemgmtsystem/EmployeemgmtsystemApplicationTests.java
@@ -1,9 +1,17 @@
 package com.sai.employeemgmtsystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@ComponentScan(basePackages = {"com.sai.employeemgmtsystem.*"})
+@EnableJpaRepositories("com.sai.employeemgmtsystem.repository")
+@EntityScan("com.sai.employeemgmtsystem.model")
+@EnableAutoConfiguration
 class EmployeemgmtsystemApplicationTests {
 
 	@Test

27a7996907896c9a7478df5bae3c46db7783006d
SaiCharanReddyMusuku <saicharanreddymusuku@gmail.com>
2023-06-04 16:24:58 -0400

    Updated main class file

--- src/main/java/com/sai/employeemgmtsystem/EmployeemgmtsystemApplication.java
+++ src/main/java/com/sai/employeemgmtsystem/EmployeemgmtsystemApplication.java
@@ -2,8 +2,14 @@ package com.sai.employeemgmtsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sai.employeemgmtsystem.*"})
+@EnableJpaRepositories("com.sai.employeemgmtsystem.repository")
+@EntityScan("com.sai.employeemgmtsystem.model")
 public class EmployeemgmtsystemApplication {
 
 	public static void main(String[] args) {
